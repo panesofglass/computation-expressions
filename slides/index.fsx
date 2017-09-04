68066582c998dd188800ee99f7f77fdf51985596
Ryan Riley <ryanriley@live.com>
2017-09-03 21:57:06 -0500

    reset

---
+++ slides/index.fsx
@@ -0,0 +1,32 @@
+(**
+- title : Extending F# through Computation Expressions
+- description : Explore patterns for extending the F# language using the computation expressions feature
+- author : Ryan Riley
+- theme : night
+- transition : default
+
+***
+
+## Extending F# through Computation Expressions
+
+### Ryan Riley
+
+***
+
+
+
+***
+
+> Three Rings for the Elven-kings under the sky,<br />
+> Seven for the Dwarf-lords in their halls of stone,<br />
+> Nine for Mortal Men doomed to die,<br />
+> One for the Dark Lord on his dark throne<br />
+> <br />
+> In the Land of Mordor where the Shadows lie.<br />
+> One Ring to rule them all, One Ring to find them,<br />
+> One Ring to bring them all and in the darkness bind them<br />
+> In the Land of Mordor where the Shadows lie.<br />
+
+— J.R.R. Tolkien's epigraph to The Lord of the Rings 
+
+*)
