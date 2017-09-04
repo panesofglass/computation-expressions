ec3519058f5ea775cda7a692784fff64d7daa70a
Ryan Riley <ryanriley@live.com>
2017-09-04 17:44:14 -0500

    Update slides through the dwarf lords

--- slides/index.fsx
+++ slides/index.fsx
@@ -13,7 +13,10 @@ System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 #load "../.paket/load/net462/main.group.fsx"
 open System
 open System.Net
+open System.Reactive
+open System.Reactive.Linq
 open FSharp.Control.Reactive
+open FSharp.Control.Reactive.Builders
 
 (**
 
@@ -53,30 +56,6 @@ open FSharp.Control.Reactive
 ' you to decide that at the end. I hope to show you that, at the
 ' very least, computation expressions can offer a useful model
 ' for writing correct programs for a given domain.
-
-***
-
-## Why Not Macros?
-
-' Since we are talking about language extensions, we must first
-' talk about macros. After all, languages in the Lisp family
-' support macros, to the extent that much of the language is built
-' in macros. What could be more powerful? In short, nothing.
-' However, F# does not provide a mechanism for writing macros,
-' and it's a fair question to ask, "Why not?"
-' Macros require a pre-compiler or a compiler plugin mechanism.
-' When you introduce something like this, you are either on
-' some other tool to handle some of the work of compiling a program.
-' This can drastically increase and complicate compile times or even
-' create very difficult-to-debug compiler crashes.
-' In addition, macros don't play very well with typed languages
-' as you are essentially relying on something typed to rely on
-' something untyped-until-generated.
-' Type Providers are the closest thing F# has to macros, but these
-' only provide types and don't extend the language.
-' Computation expressions don't carry the full power of macros,
-' but they cover the most important use cases to the point you won't
-' likely miss macros.
 
 ***
 
@@ -172,6 +151,11 @@ async {
 ' Now that we've discussed our motivation and looked
 ' at some examples, let's move into the patterns for
 ' building computation expressions.
+' I'm sure you already noticed that the previous examples
+' use wrapper, or container, types. In other words, these
+' are generic types that contain values. Computation expressions
+' provide us a mechanism for working with these container types.
+' But not all containers are created equal.
 
 ***
 
@@ -199,7 +183,7 @@ async {
 
 ***
 
-### One for the Dark Lord on his dark throne
+## One for the Dark Lord on his dark throne
 
 ' We'll come back to this one in just a bit. The motivating
 ' factor for many when first encroaching into computation
@@ -210,14 +194,18 @@ async {
 
 ***
 
-### Nine for Mortal Men doomed to die
+## Nine for Mortal Men doomed to die
 
-' Uplifting, isn't it? To say that this talk is not about monads
-' isn't entirely true; it's just they aren't the main point. We've
-' already looked at two, though it's really not important that you
-' fully understand what is a monad (or a monoid, applicative, etc.)
-' though it certainly won't hurt you to understand why they are
-' valuable and how they might help you reason about your code.
+' The Nine became the Nazgul, slaves to the Dark Lord Sauron.
+' I found this to be true of me when first trying to understand
+' computation expressions, and it blinded me for years to seeing
+' and understanding their potential.
+' To say that this talk is not about monads isn't entirely true;
+' it's just they aren't the main point. We've already looked at two,
+' though it's really not important that you fully understand what
+' is a monad (or a monoid, applicative, etc.) though it certainly
+' won't hurt you to understand why they are valuable and how they
+' might help you reason about your code.
 ' Let's look at another example using `Option<'T>`.
 
 ***
@@ -225,11 +213,11 @@ async {
 ### OptionBuilder
 *)
 
-type OptionBuilderCore() =
+type OptionMonad() =
     member __.Bind(m, f) = Option.bind f m
     member __.Return(x) = Some x
 
-let opt = OptionBuilderCore()
+let opt = OptionMonad()
 
 (**
 
@@ -254,7 +242,7 @@ type OptionBuilder() =
     member __.Return(x) = Some x
     member __.ReturnFrom(m: 'T option) = m
     member __.Zero() = Some ()
-    member __.Combine(m, f: unit -> 'T option) = Option.bind f m
+    member __.Combine(m, f: unit -> _) = Option.bind f m
     member __.Delay(f: unit -> 'T) = f
     member __.Run(f) = f()
 
@@ -323,13 +311,14 @@ else None
 
 ***
 
-## Back to the One Ring
+### Back to the One Ring
 
 * [FSharpPlus](https://github.com/gusty/FSharpPlus)
 * [FSharpx.Extras](https://github.com/fsprojects/FSharpx.Extras/tree/master/src/FSharpx.Extras/ComputationExpressions)
 * [Higher](https://github.com/palladin/Higher)
 
-' You may get caught in the trap of falling of falling into this pattern.
+' Much like the Nazgul, seduced by Sauron's power through the One Ring,
+' you may get caught in the trap of trying to implement the one, true monad CE.
 ' Many computations can be written almost exactly alike, assuming they are
 ' monads. However, F# is not a purely functional, lazy language like Haskell,
 ' and the implementations cannot be implemented exactly alike.
@@ -342,25 +331,29 @@ else None
 
 ***
 
-## [Classes for the Masses](https://github.com/MattoWindsor91/visualfsharp/blob/hackathon-vs/examples/fsconcepts.md)
+## Seven for the Dwarf-lords in their halls of stone
 
-' Incidentally, type classes, which Haskell uses to encode monads and other
-' container types, have a proven encoding in F# and may be coming in a future
-' release. Hopefully this will alleviate some of the pit of computation-
-' expressions-as-monad-encodings that many fall into.
+' Okay, let's move towards the light. Or at least a different kind of
+' darkness. The rings of the dwarves don't factor into Tolkien's stories
+' much, so I can't make a great analogy, but let's assume they were more
+' powerful than the rings for men. In that case, I would relate them to
+' the query expressions introduced in F# 3.0.
 
 ***
 
-## Seven for the Dwarf-lords in their halls of stone
+### QueryBuilder
 
 *)
 
+(*** define-output: query-builder ***)
 query {
     for x in 1..10 do
     for y in 11..20 do
     where (x % 2 = 1 && y % 2 = 0)
     select (x + y)
 }
+
+(*** include-it: query-builder ***)
 
 (**
 
@@ -373,11 +366,9 @@ query {
 
 ***
 
-## [FSharp.Control.Reactive](http://fsprojects.github.io/FSharp.Control.Reactive/)
+### [FSharp.Control.Reactive](http://fsprojects.github.io/FSharp.Control.Reactive/)
 
 *)
-
-open FSharp.Control.Reactive.Builders
 
 (*** define-output: rxquery-zip ***)
 rxquery {
@@ -395,10 +386,145 @@ rxquery {
 ' to the Observable module to fit the F# style. It also provides an `observe`
 ' computation expression for the monadic operations and `rxquery` for query
 ' expressions. 
+' The `for` is one of the standard operators used in computation expressions.
+' However, `zip` and `select` are not standard. They are not part of the core
+' F# keywords. How do they appear within the language?
 
 ***
 
-## Extending Existing Builders
+### RxQueryBuilder Select and Zip
+
+*)
+
+type RxQueryBuilder with
+
+    [<CustomOperation("select", AllowIntoPattern=true)>]
+    member __.Select (s:IObservable<_>, 
+                      [<ProjectionParameter>] selector : _ -> _) =
+        s.Select(selector)
+
+    [<CustomOperation("zip", IsLikeZip=true)>]
+    member __.Zip (s1:IObservable<_>,
+                   s2:IObservable<_>,
+                   [<ProjectionParameter>] resultSelector : _ -> _) =
+        s1.Zip(s2, new Func<_,_,_>(resultSelector))
+
+(**
+
+' These are two custom operations introduced in F# 3.0 with the
+' query expressions. Note that we give the name in the `CustomOperationAttribute`.
+' The attribute accepts additional parameters, such as `AllowIntoPattern`,
+' `IsLikeZip`, etc. The full set of options is outside the scope of this talk,
+' but you can find them listed in the Microsoft docs online and in the full
+' implementation of the `RxQueryBuilder`.
+' The FSharp.Control.Reactive library made an interesting choice in providing
+' both the `observe` CE and the `rxquery` QE. There's no reason you cannot mix
+' the two in one builder. It turns out, you can also extend an existing builder
+' with new custom operations.
+
+***
+
+### Extending Existing Builders
+
+*)
+
+type FSharp.Control.AsyncBuilder with
+
+    [<CustomOperation("and!", IsLikeZip=true)>]
+    member __.Merge(x, y, f) =
+        async {
+            let! token = Async.CancellationToken
+            let! x' = Async.StartChildAsTask x
+            let! y' = Async.StartChildAsTask y
+            do System.Threading.Tasks.Task.WaitAll([|x';y'|], cancellationToken = token)
+            let! x'' = Async.AwaitTask x'
+            let! y'' = Async.AwaitTask y'
+            return f x'' y''
+        }
+
+    member __.For(m, f) = __.Bind(m, f)
+
+(**
+
+' If you ever wished you could run two or more async computations in parallel
+' within an async computation expression, now you know you can by extending the
+' AsyncBuilder provided with FSharp.Core.
+
+***
+
+### Async Applicative Example
+
+' Jump to VSCode to run this in FSI, as the code snippet is too long.
+' The code compares awaiting two sleeps and shows that the applicative,
+' which does not require awaiting the first value, can freely run in
+' parallel, whereas the monad version using `let!` must wait for each
+' in turn.
+' We could argue about the best syntax, but this version uses that proposed
+' in the VisualFSharp issues: https://github.com/fsharp/fslang-suggestions/issues/579
+
+*)
+
+(*** hide ***)
+
+let a (sw:Diagnostics.Stopwatch) = async {
+    printfn "starting a %O" sw.ElapsedMilliseconds
+    do! Async.Sleep 1000
+    printfn "returning a %O" sw.ElapsedMilliseconds
+    return 1
+}
+
+let b (sw:Diagnostics.Stopwatch) = async {
+    printfn "starting b %O" sw.ElapsedMilliseconds
+    do! Async.Sleep 500
+    do printfn "returning b %O" sw.ElapsedMilliseconds
+    return 2
+}
+
+let comp sw = async {
+    for x in a sw do
+    ``and!`` y in b sw
+    return x + y
+}
+
+let compBind sw = async {
+    let! x = a sw
+    let! y = b sw
+    return x + y
+}
+
+let sw = Diagnostics.Stopwatch.StartNew()
+let result = comp sw |> Async.RunSynchronously
+sw.Stop()
+printfn "comp ran in %Oms with result %i" sw.ElapsedMilliseconds result
+
+// Compare with
+sw.Reset()
+sw.Start()
+let resultBind = compBind sw |> Async.RunSynchronously
+sw.Stop()
+printfn "compBind ran in %Oms with result %i" sw.ElapsedMilliseconds resultBind
+
+let c (sw:Diagnostics.Stopwatch) = async {
+    printfn "starting c %O" sw.ElapsedMilliseconds
+    do! Async.Sleep 1500
+    do printfn "returning c %O" sw.ElapsedMilliseconds
+    return 3
+}
+
+let comp3 sw = async {
+    for x in a sw do
+    ``and!`` y in b sw
+    ``and!`` z in c sw
+    return x + y + z
+}
+
+sw.Reset()
+sw.Start()
+let result3 = comp3 sw |> Async.RunSynchronously
+sw.Stop()
+printfn "comp3 ran in %Oms with result %i" sw.ElapsedMilliseconds result3
+
+(**
 
 ***
 
@@ -419,6 +545,41 @@ rxquery {
 
 ***
 
+## Why Not Macros?
+
+' Since we are talking about language extensions, we must first
+' talk about macros. After all, languages in the Lisp family
+' support macros, to the extent that much of the language is built
+' in macros. What could be more powerful? In short, nothing.
+' However, F# does not provide a mechanism for writing macros,
+' and it's a fair question to ask, "Why not?"
+' Macros require a pre-compiler or a compiler plugin mechanism.
+' When you introduce something like this, you are either on
+' some other tool to handle some of the work of compiling a program.
+' This can drastically increase and complicate compile times or even
+' create very difficult-to-debug compiler crashes.
+' In addition, macros don't play very well with typed languages
+' as you are essentially relying on something typed to rely on
+' something untyped-until-generated.
+' Type Providers are the closest thing F# has to macros, but these
+' only provide types and don't extend the language.
+' Computation expressions don't carry the full power of macros,
+' but they cover the most important use cases to the point you won't
+' likely miss macros.
+
+***
+
+## Will F# ever get Type Classes?
+
+### [Classes for the Masses](https://github.com/MattoWindsor91/visualfsharp/blob/hackathon-vs/examples/fsconcepts.md)
+
+' Incidentally, type classes, which Haskell uses to encode monads and other
+' container types, have a proven encoding in F# and may be coming in a future
+' release. Hopefully this will alleviate some of the pit of computation-
+' expressions-as-monad-encodings that many fall into.
+
+***
+
 ## References
 
 1. [Computation Expressions](https://docs.microsoft.com/en-us/dotnet/fsharp/language-reference/computation-expressions)
@@ -427,6 +588,7 @@ rxquery {
 4. [Introducing F# Asynchronous Workflows](https://blogs.msdn.microsoft.com/dsyme/2007/10/10/introducing-f-asynchronous-workflows/)
 5. [The F# Computation Expression Zoo](http://tomasp.net/academic/papers/computation-zoo/computation-zoo.pdf)
 6. [F# for fun and profit](https://fsharpforfunandprofit.com/series/computation-expressions.html)
+7. [Support let! .. and... for applicative functors](https://github.com/fsharp/fslang-suggestions/issues/579)
 
 ***
 *)
